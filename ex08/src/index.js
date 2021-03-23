82c73a05080b2febb50c989ad189ddd7ea2cfdc5
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-23 10:38:47 +0100

    Added all exercises from Day15

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+// Only changes code below this line
+const sum = (num1, num2 = 1) => {
+    return num1 + num2;
+}
+// Only changes code below this line
+
+console.log(sum(4, 6)); //change this line
+console.log(sum(3)); //change this line
+module.exports = sum;
